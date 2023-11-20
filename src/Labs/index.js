50c34fa0d5bdc1697fec04fcc595da2992d5681b
nagiaboushaca <nagishaca@gmail.com>
2023-11-20 04:34:49 -0500

    lab done

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,5 +1,6 @@
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import Assignment5 from "./a5";
 import { Route, Routes, Navigate } from "react-router";
 import JavaScript from "./a3/JavaScript";
 import Nav from "../Nav";
@@ -23,6 +24,8 @@ function Labs() {
         element={<Assignment3/>}/>
        <Route path="a4"
         element={<Assignment4/>}/>
+        <Route path="a5"
+        element={<Assignment5/>}/>
      </Routes>
       </div>
       </Provider>
